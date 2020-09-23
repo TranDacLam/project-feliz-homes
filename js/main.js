57466ea3057546bf5b1aa402aa0e0fe291a68d0d
Tran Dac Lam <lamtd@fireapps.vn>
2020-09-23 17:44:41 +0700

    #1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
- $('.header').height($(window).height());
+	$('.header').height($(window).height());
 
- $(".navbar a").click(function(){
- 	$("body,html").animate({
- 		scrollTop:$("#" + $(this).data('value')).offset().top
- 	},1000)
-  
- })
+	$(".navbar a").click(function(){
+		$("body,html").animate({
+			scrollTop:$("#" + $(this).data('value')).offset().top
+		},1000)
+	
+	})
 
- $('.your-class').slick();
+	$('.your-class').slick();
+
+	window.onscroll = function() {myFunction()};
+
+	// Get the header
+	var header = document.getElementById("landing-nav");
+
+	// Get the offset position of the navbar
+	var sticky = header.offsetTop;
+
+	// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+		console.log(sticky, window.pageYOffset)
+		if (window.pageYOffset > sticky) {
+			header.classList.add("sticky-top");
+		} else {
+			header.classList.remove("sticky-top");
+		}
+	}
 
 })
