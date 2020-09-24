7f1afa2955cf2856062a28559dc9ba63eae4bf27
Tran Dac Lam <lamtd@fireapps.vn>
2020-09-24 17:49:45 +0700

    #3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-$(document).ready(function(){
-	$('.header').height($(window).height());
+$(function(){
 
-	$(".navbar a").click(function(){
-		$("body,html").animate({
-			scrollTop:$("#" + $(this).data('value')).offset().top
-		},1000)
+	new WOW().init();
+
 	
-	})
+	// $('.header').height($(window).height());
 
-	$('.your-class').slick();
+	// $(".navbar a").click(function(){
+	// 	$("body,html").animate({
+	// 		scrollTop:$("#" + $(this).data('value')).offset().top
+	// 	},1000)
+	
+	// })
+
+	// $('.your-class').slick();
 
 	window.onscroll = function() {myFunction()};
 
@@ -16,11 +20,12 @@ $(document).ready(function(){
 	var header = document.getElementById("landing-nav");
 
 	// Get the offset position of the navbar
-	var sticky = header.offsetTop + 100;
+	var sticky = header.offsetTop;
+
+	myFunction()
 
 	// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 	function myFunction() {
-		console.log(sticky, window.pageYOffset)
 		if (window.pageYOffset > sticky) {
 			header.classList.add("sticky-top");
 		} else {
