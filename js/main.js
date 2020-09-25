f268be4c1e4f62ff2cf2e70e712abf25ec27c6fb
Tran Dac Lam <lamtd@fireapps.vn>
2020-09-25 17:53:00 +0700

    #4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,71 @@
 $(function(){
 
 	new WOW().init();
+	let navText = [
+		`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+			<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+		</svg>`,
+		`<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+			<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+		</svg>`
+	];
+	
+	$('.utilities__list').owlCarousel({
+		items:1,
+		nav: true,
+		dots: false,
+		loop:true,
+		navText: navText,
+		responsive:{
+			
+		}
+	});
 
+	$('.ground__list').owlCarousel({
+		items:1,
+		loop:true,
+		margin:10,
+		stagePadding: 200,
+		nav: true,
+		navText: navText,
+		responsive:{
+			
+		}
+	});
+
+	$('.apartment__list').owlCarousel({
+		items:1,
+		nav: true,
+		dots: false,
+		loop:true,
+		navText: navText,
+		responsive:{
+			
+		}
+	});
+
+	$('.furniture__list').owlCarousel({
+		items:1,
+		loop:true,
+		margin:10,
+		stagePadding: 200,
+		nav: true,
+		navText: navText,
+		responsive:{
+			
+		}
+	});
+
+	$('.news__list').owlCarousel({
+		items:3,
+		loop:true,
+		margin:60,
+		nav: true,
+		navText: [`<img src="images/arrow-1.png" />`, `<img src="images/arrow-2.png" />`],
+		responsive:{
+			
+		}
+	});
 	
 	// $('.header').height($(window).height());
 
@@ -11,8 +75,6 @@ $(function(){
 	// 	},1000)
 	
 	// })
-
-	// $('.your-class').slick();
 
 	window.onscroll = function() {myFunction()};
 
